df6e040afa72b59f8294cf2816945aa55a28d056
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-06-27 19:59:09 -0300

    fix: retornando uma dica aleatoria

--- src/controller/tips-controller.js
+++ src/controller/tips-controller.js
@@ -1,5 +1,8 @@
 export function tips(app, bd){
     app.get('/tips', function (req, res) {
-        res.send(bd.tip);
+        const tip1 = bd.tip;
+        const tipRamdom = Math.floor(Math.random() * tip1.length);
+        res.send(tip1[tipRamdom]); // resultado aleatório
+        //res.send(bd.tip);
       })
 }
